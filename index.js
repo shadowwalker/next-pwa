17db7526ea1305197bf99c55aa678e3a4f3ab957
Wei Wang <weiwio@outlook.com>
2019-04-25 20:34:00 -0700

    Fix issues

--- index.js
+++ index.js
@@ -4,9 +4,81 @@ const CleanPlugin = require('clean-webpack-plugin')
 const ReplacePlugin = require('replace-in-file-webpack-plugin')
 const WorkboxPlugin = require('workbox-webpack-plugin')
 
+const defaultCache = [{
+  urlPattern: /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
+  handler: 'CacheFirst',
+  options: {
+    cacheName: 'google-fonts',
+    expiration: {
+      maxEntries: 4,
+      maxAgeSeconds: 365 * 24 * 60 * 60  // 365 days
+    }
+  }
+}, {
+  urlPattern: /^https:\/\/use\.fontawesome\.com\/releases\/.*/i,
+  handler: 'CacheFirst',
+  options: {
+    cacheName: 'font-awesome',
+    expiration: {
+      maxEntries: 1,
+      maxAgeSeconds: 365 * 24 * 60 * 60  // 365 days
+    }
+  }
+}, {
+  urlPattern: /\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,
+  handler: 'StaleWhileRevalidate',
+  options: {
+    cacheName: 'static-font-assets',
+    expiration: {
+      maxEntries: 4,
+      maxAgeSeconds: 7 * 24 * 60 * 60  // 7 days
+    }
+  }
+}, {
+  urlPattern: /\.(?:jpg|jpeg|gif|png|svg|ico)$/i,
+  handler: 'StaleWhileRevalidate',
+  options: {
+    cacheName: 'static-image-assets',
+    expiration: {
+      maxEntries: 64,
+      maxAgeSeconds: 24 * 60 * 60  // 24 hours
+    }
+  }
+}, {
+  urlPattern: /\.(?:js)$/i,
+  handler: 'StaleWhileRevalidate',
+  options: {
+    cacheName: 'static-js-assets',
+    expiration: {
+      maxEntries: 16,
+      maxAgeSeconds: 24 * 60 * 60  // 24 hours
+    }
+  }
+}, {
+  urlPattern: /\.(?:css|less)$/i,
+  handler: 'StaleWhileRevalidate',
+  options: {
+    cacheName: 'static-style-assets',
+    expiration: {
+      maxEntries: 16,
+      maxAgeSeconds: 24 * 60 * 60  // 24 hours
+    }
+  }
+}, {
+  urlPattern: /.*/i,
+  handler: 'StaleWhileRevalidate',
+  options: {
+    cacheName: 'others',
+    expiration: {
+      maxEntries: 16,
+      maxAgeSeconds: 24 * 60 * 60  // 24 hours
+    }
+  }
+}]
+
 const registerSW = (config) => {
   registerScript = path.join(__dirname, 'register.js')
-  console.log(`> [PWA] use the script for register: ${path.resolve(registerSW)}`)
+  console.log(`> [PWA] auto register service worker using: ${path.resolve(registerScript)}`)
 
   const entry = config.entry
   config.entry = async () => entry().then(entries => {
@@ -16,9 +88,6 @@ const registerSW = (config) => {
     return entries
   })
 }
-
-// For workbox configurations:
-// https://developers.google.com/web/tools/workbox/modules/workbox-webpack-plugin
 
 module.exports = (nextConfig = {}) => ({
   ...nextConfig,
@@ -31,15 +100,19 @@ module.exports = (nextConfig = {}) => ({
       pwa = {}
     } = options
 
+    // For workbox configurations:
+    // https://developers.google.com/web/tools/workbox/modules/workbox-webpack-plugin
     const {
-      disable = !options.dev,
+      disable = options.dev,
       register = true,
       dest = distDir,
       sw = '/sw.js',
       scope = '/',
+      runtimeCaching = defaultCache,
       ...workbox
     } = pwa
 
+    console.log(`[PWA] compile for ${options.isServer ? 'server' : 'static (client)'}`)
     if (!disable) {
       const _sw = sw.startsWith('/') ? sw : `/${sw}`
       const _dest = path.join(options.dir, dest)
@@ -50,11 +123,10 @@ module.exports = (nextConfig = {}) => ({
       }))
 
       if(register) {
-        console.log(`> [PWA] auto register service worker in main.js on ${options.isServer ? '[server]' : '[static (client)]'}`)
         registerSW(config)
       } else {
-        console.log(`> [PWA] auto register service worker is DISABLED on ${options.isServer ? '[server]' : '[static (client)]'}`)
-        console.log(`> [PWA] make sure to implement code to register service worker`)
+        console.log(`> [PWA] auto register service worker is disabled`)
+        console.log(`> [PWA] make sure to handle register service worker yourself`)
       }
 
       if (!options.isServer) {
@@ -94,6 +166,10 @@ module.exports = (nextConfig = {}) => ({
             })
           )
         } else {
+          if (typeof runtimeCaching === 'function') {
+            runtimeCaching = runtimeCaching(defaultCache)
+          }
+
           console.log('> [PWA] generating new service worker', path.join(_dest, sw))
           config.plugins.push(
             new WorkboxPlugin.GenerateSW({
@@ -101,77 +177,7 @@ module.exports = (nextConfig = {}) => ({
               skipWaiting: true,
               clientsClaim: true,
               cleanupOutdatedCaches: true,
-              runtimeCaching: [{
-                urlPattern: /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
-                handler: 'CacheFirst',
-                options: {
-                  cacheName: 'google-fonts',
-                  expiration: {
-                    maxEntries: 4,
-                    maxAgeSeconds: 365 * 24 * 60 * 60  // 365 days
-                  }
-                }
-              }, {
-                urlPattern: /^https:\/\/use\.fontawesome\.com\/releases\/.*/i,
-                handler: 'CacheFirst',
-                options: {
-                  cacheName: 'font-awesome',
-                  expiration: {
-                    maxEntries: 1,
-                    maxAgeSeconds: 365 * 24 * 60 * 60  // 365 days
-                  }
-                }
-              }, {
-                urlPattern: /\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,
-                handler: 'StaleWhileRevalidate',
-                options: {
-                  cacheName: 'static-font-assets',
-                  expiration: {
-                    maxEntries: 4,
-                    maxAgeSeconds: 7 * 24 * 60 * 60  // 7 days
-                  }
-                }
-              }, {
-                urlPattern: /\.(?:jpg|jpeg|gif|png|svg|ico)$/i,
-                handler: 'StaleWhileRevalidate',
-                options: {
-                  cacheName: 'static-image-assets',
-                  expiration: {
-                    maxEntries: 64,
-                    maxAgeSeconds: 24 * 60 * 60  // 24 hours
-                  }
-                }
-              }, {
-                urlPattern: /\.(?:js)$/i,
-                handler: 'StaleWhileRevalidate',
-                options: {
-                  cacheName: 'static-js-assets',
-                  expiration: {
-                    maxEntries: 16,
-                    maxAgeSeconds: 24 * 60 * 60  // 24 hours
-                  }
-                }
-              }, {
-                urlPattern: /\.(?:css|less)$/i,
-                handler: 'StaleWhileRevalidate',
-                options: {
-                  cacheName: 'static-style-assets',
-                  expiration: {
-                    maxEntries: 16,
-                    maxAgeSeconds: 24 * 60 * 60  // 24 hours
-                  }
-                }
-              }, {
-                urlPattern: /.*/i,
-                handler: 'StaleWhileRevalidate',
-                options: {
-                  cacheName: 'others',
-                  expiration: {
-                    maxEntries: 16,
-                    maxAgeSeconds: 24 * 60 * 60  // 24 hours
-                  }
-                }
-              }],
+              runtimeCaching,
               ...workbox
             })
           )
@@ -196,6 +202,8 @@ module.exports = (nextConfig = {}) => ({
           }]
         }]))
       }
+    } else {
+      console.log('[PWA] PWA support currently disabled')
     }
 
     if (typeof nextConfig.webpack === 'function') {
