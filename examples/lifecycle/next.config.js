99273a143f6d8b933324fa36c04d2a92b5bda5a0
Shadow Walker <weiwio@outlook.com>
2020-02-15 11:17:03 -0800

    Update lifecycle example

--- examples/lifecycle/next.config.js
+++ examples/lifecycle/next.config.js
@@ -3,6 +3,7 @@ const withPWA = require('next-pwa')
 module.exports = withPWA({
   pwa: {
     dest: 'public',
-    register: false
+    register: false,
+    skipWaiting: false
   }
 })
--- examples/lifecycle/pages/index.js
+++ examples/lifecycle/pages/index.js
@@ -23,9 +23,22 @@ export default () => {
         console.log(event)
       })
 
+      // A common UX pattern for progressive web apps is to show a banner when a service worker has updated and waiting to install.
+      // NOTE: set skipWaiting to false in next.config.js pwa object
+      // https://developers.google.com/web/tools/workbox/guides/advanced-recipes#offer_a_page_reload_for_users
+      window.workbox.addEventListener('waiting', event => {
+        if (confirm('A new version is installed, reload to use the new version immediately?')) {
+          window.workbox.addEventListener('controlling', event => {
+            window.location.reload()
+          })
+          window.workbox.messageSW({type: 'SKIP_WAITING'})
+        } else {
+          // User rejected, new verion will be automatically load when user open the app next time.
+        }
+      })
+
       // ISSUE - this is not working as expected, why?
-      // I could only make message event listenser work when I 
-      // manually add this listenser into sw.js file
+      // I could only make message event listenser work when I manually add this listenser into sw.js file
       window.workbox.addEventListener('message', event => {
         console.log(`Event ${event.type} is triggered.`)
         console.log(event)
@@ -33,11 +46,6 @@ export default () => {
 
       /*
       window.workbox.addEventListener('redundant', event => {
-        console.log(`Event ${event.type} is triggered.`)
-        console.log(event)
-      })
-
-      window.workbox.addEventListener('waiting', event => {
         console.log(`Event ${event.type} is triggered.`)
         console.log(event)
       })
