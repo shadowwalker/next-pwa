c48ef110360d0138ad2dacd82ab96964e3da2daf
Shadow Walker <weiwio@outlook.com>
2020-10-26 18:57:31 -0700

    Update example

--- examples/custom-worker/worker/index.js
+++ examples/custom-worker/worker/index.js
@@ -1,5 +1,10 @@
 'use strict'
 
+// To disable all workbox logging during development, you can set self.__WB_DISABLE_DEV_LOGS to true
+// https://developers.google.com/web/tools/workbox/guides/configure-workbox#disable_logging
+//
+// self.__WB_DISABLE_DEV_LOGS = true
+
 const util = require('./util')
 
 util()
