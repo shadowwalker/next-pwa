46743f8e1a285a7dd8486bea5bb90f9d26b54e71
Tevinthuku <tevinthuku@gmail.com>
2021-04-12 11:14:04 +0300

    split the google-fonts strategies

--- cache.js
+++ cache.js
@@ -3,16 +3,27 @@
 // Workbox RuntimeCaching config: https://developers.google.com/web/tools/workbox/reference-docs/latest/module-workbox-build#.RuntimeCachingEntry
 module.exports = [
   {
-    urlPattern: /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
+    urlPattern: /^https:\/\/fonts\.(?:gstatic)\.com\/.*/i,
     handler: 'CacheFirst',
     options: {
-      cacheName: 'google-fonts',
+      cacheName: 'google-fonts-webfonts',
       expiration: {
         maxEntries: 4,
-        maxAgeSeconds: 365 * 24 * 60 * 60 // 365 days
-      }
-    }
+        maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
+      },
+    },
   },
+  {
+    urlPattern: /^https:\/\/fonts\.(?:googleapis)\.com\/.*/i,
+    handler: 'StaleWhileRevalidate',
+    options: {
+      cacheName: 'google-fonts-stylesheets',
+      expiration: {
+        maxEntries: 4,
+        maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
+      },
+    },
+  }
   {
     urlPattern: /\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,
     handler: 'StaleWhileRevalidate',
--- examples/lifecycle/next.config.js
+++ examples/lifecycle/next.config.js
@@ -3,7 +3,6 @@ const withPWA = require('next-pwa')
 // change start-url cache strategy, so that we can prompt user to reload when
 // new version available, instead of showing new version directly
 const runtimeCaching = require('next-pwa/cache')
-runtimeCaching[0].handler = 'StaleWhileRevalidate'
 
 module.exports = withPWA({
   pwa: {
