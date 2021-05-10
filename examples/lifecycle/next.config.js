7691765852b99389d8600dc962837986af41a2ea
Tevinthuku <tevinthuku@gmail.com>
2021-05-10 12:00:25 +0300

    fix conflict

--- examples/lifecycle/next.config.js
+++ examples/lifecycle/next.config.js
@@ -1,14 +1,9 @@
 const withPWA = require('next-pwa')
 
-// change start-url cache strategy, so that we can prompt user to reload when
-// new version available, instead of showing new version directly
-const runtimeCaching = require('next-pwa/cache')
-
 module.exports = withPWA({
   pwa: {
     dest: 'public',
     register: false,
-    skipWaiting: false,
-    runtimeCaching
+    skipWaiting: false
   }
 })
