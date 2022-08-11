5a2cf63f81fa81c244af9bd3b26ea7db7f2b40d9
Marco Franssen <marco.franssen@gmail.com>
2022-08-11 12:49:54 +0200

    Remove unused variable

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ module.exports = (nextConfig = {}) => {
         webpack,
         buildId,
         dev,
-        config: { distDir = '.next', pwa = {}, pageExtensions = ['tsx', 'ts', 'jsx', 'js', 'mdx'], experimental = {} }
+        config: { distDir = '.next', pageExtensions = ['tsx', 'ts', 'jsx', 'js', 'mdx'], experimental = {} },
       } = options
 
       let basePath = options.config.basePath
